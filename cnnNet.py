d20bd972b390a01ab8a31039c70983d777d961a3
Divyeshpratap <singh_divyesh@outlook.com>
2024-07-19 05:21:59 -0400

    added basic CNN code

---
+++ cnnNet.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+class CNN(nn.Module):
+
+    def __init__(self, in_channels=1, num_classes=10):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels=8, kernel_size = (3,3), stride=(1,1), padding=(1,1))
+        self.pool = nn.MaxPool2d(kernel_size = (2,2), stride = (2,2))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride= (1,1), padding=(1,1))
+        self.fc1 = nn.Linear(16*7*7, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
+        return x
+    
+# model = CNN(3, 10)
+
+# x = torch.rand((100, 3, 28, 28))
+# print(f'output shape is {model(x).shape}')
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+in_channels = 1
+num_classes = 10
+learning_rate = 3e-4
+batch_size = 64
+num_epochs = 2
+
+train_dataset = datasets.MNIST(root='/datasets', train = True, transform = transforms.ToTensor(), download = True)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+test_dataset = datasets.MNIST(root='/datasets', train = False, transform = transforms.ToTensor(), download = True)
+test_loader = DataLoader(test_dataset, shuffle=True, batch_size=batch_size)
+
+model = CNN(in_channels, num_classes).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.AdamW(model.parameters(), lr =learning_rate)
+
+print(f'Starting training')
+for epoch in range(num_epochs):
+    for data, targets in train_loader:
+        data = data.to(device)
+        targets = targets.to(device)
+
+        logits = model(data)
+        loss = criterion(logits, targets)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+@torch.no_grad()
+def check_accuracy(loader, model):
+    num_correct = 0
+    num_steps = 0
+    if loader.dataset.train:
+        print(f'evaluating on train dataset')
+    else:
+        print(f'evaulating on test dataset')
+    model.eval()
+    for x, y in loader:
+        x = x.to(device)
+        y = y.to(device)
+        logits = model(x)
+        _, predictions = logits.max(1)
+        num_correct += (predictions == y).sum()
+        num_steps += y.shape[0]
+    print(f'Accuracy during evaluation phase is {((num_correct/num_steps)*100):.2f} percent')
+    model.train()
+
+check_accuracy(train_loader, model)
+check_accuracy(test_loader, model)
+
+
